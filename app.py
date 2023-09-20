931d07924d45561738b80329ea7e05d04bf80ade
alsfkl222 <alskfl222@kakao.com>
2023-09-20 17:42:56 +0900

    Fix: add status, change port

--- app.py
+++ app.py
@@ -3,27 +3,39 @@ import datetime
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from request import request_worker, request_queue
 
+
 class StreamViewServer:
     def __init__(self):
         self.app = FastAPI()
         self.controller_client = {}
         self.app.websocket("/ws")(self.websocket_endpoint)
+        self.status = {
+            "category": {
+                "main": "charactor",
+                "sub": "current",
+            },
+            "data": {
+                "name": "1111"
+            }
+        }
 
     async def websocket_endpoint(self, websocket: WebSocket):
         await websocket.accept()
         while True:
             try:
                 json_data = await websocket.receive_json()
-                print(f"[APP]\t\t: {datetime.datetime.now():%Y-%m-%d %H:%M:%S} : {json_data}")
-                request_queue.put((self, websocket, json_data)) 
+                print(
+                    f"[APP]\t\t: {datetime.datetime.now():%Y-%m-%d %H:%M:%S} : {json_data}")
+                request_queue.put((self, websocket, json_data))
             except WebSocketDisconnect:
                 break
 
     def run(self):
         threading.Thread(target=request_worker, daemon=True).start()
-        
+
         import uvicorn
-        uvicorn.run(self.app, host="0.0.0.0", port=8000)
+        uvicorn.run(self.app, host="0.0.0.0", port=5000)
+
 
 if __name__ == "__main__":
     server = StreamViewServer()
