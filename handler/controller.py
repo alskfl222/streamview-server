b6522ba475d7ba55c38cb955c271f5b11e01e9e8
alskfl222 <alskfl222@kakao.com>
2023-10-20 18:28:31 +0900

    Feat: use mongoDB

--- handler/controller.py
+++ handler/controller.py
@@ -1,5 +1,5 @@
-import datetime
 
+import datetime
 
 async def controller_handler(server, websocket, json_data):
     check_message = f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S} : {json_data}"
--- request.py
+++ request.py
@@ -3,7 +3,21 @@ import queue
 import time
 import datetime
 import asyncio
+import os
+import pymongo
+import certifi
+from dotenv import load_dotenv
 from handler.controller import controller_handler
+
+load_dotenv()
+ENV = os.getenv("ENV")
+DBID = os.getenv("MONGODB_ID")
+DBPW = os.getenv("MONGODB_PW")
+atlas_link = f"mongodb+srv://{DBID}:{DBPW}@info.syvdo.mongodb.net/info?retryWrites=true&w=majority"
+
+dbclient = pymongo.MongoClient(atlas_link, tlsCAFile=certifi.where())
+db = dbclient["StreamView"]
+col_log = db.Log
 
 request_queue = queue.Queue()
 
@@ -31,7 +45,10 @@ def request_worker():
                 loop.run_until_complete(websocket.send_json(response))
             else:
                 sender = json_data["sender"]
-                if sender == "controller":
+                if not sender['type']['user']:
+                    continue
+                col_log.insert_one(json_data)
+                if sender['type'] == 'controller':
                     loop.run_until_complete(async_controller_handler(server, websocket, json_data))
                 time.sleep(1)
         except:
