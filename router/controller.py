01baf8325f9bfe2965eeb1317d0ef14aa827f732
alskfl222 <alskfl222@kakao.com>
2023-10-31 17:03:48 +0900

    Feat: add and get todo

--- router/controller.py
+++ router/controller.py
@@ -1,9 +1,9 @@
-from typing import List, Dict, Any
-import datetime
-import pymongo
-from fastapi import APIRouter, Depends
+from typing import Optional, List, Dict, Any
+from datetime import datetime
+from fastapi import APIRouter, Depends, Query, HTTPException
 from pydantic import BaseModel
 from firebase_admin import auth
+import pymongo
 from dependency import get_current_user
 from db import db
 from util import remove_objectId
@@ -12,8 +12,19 @@ from util import remove_objectId
 class CurrentModel(BaseModel):
     current: Dict[str, Any]
 
-class TodoModel(BaseModel):
-    todos: List[Dict[str, Any]]
+class TodoItem(BaseModel):
+    id: str
+    type: str
+    kind: str
+    activity: Optional[Dict[str, Any]] = None
+    addedTime: str
+    plannedStartTime: Optional[str] = None
+    actualStartTime: Optional[str] = None
+    endTime: Optional[str] = None
+
+class TodoData(BaseModel):
+    date: str
+    todos: List[TodoItem]
 
 
 router = APIRouter(prefix="/controller")
@@ -21,7 +32,7 @@ router = APIRouter(prefix="/controller")
 
 @router.get("/")
 def get_controller(user: auth.UserRecord = Depends(get_current_user)):
-    col_current = db["current"]
+    col_current = db.current
     try:
         doc_current = (
             col_current.find({"uid": user.uid})
@@ -32,10 +43,10 @@ def get_controller(user: auth.UserRecord = Depends(get_current_user)):
     except:
         doc_current = {
             "uid": user.uid,
-            "time": datetime.datetime.utcnow(),
+            "time": datetime.now(),
             "current": {
                 "display": "todo",
-                "date": datetime.datetime.utcnow(),
+                "date": datetime.now(),
             },
         }
         col_current.insert_one(doc_current)
@@ -50,7 +61,7 @@ def insert_current(
 
     doc_current = {
         "uid": user.uid,
-        "time": datetime.datetime.utcnow(),
+        "time": datetime.now(),
         "current": data.current,
     }
 
@@ -65,16 +76,50 @@ def insert_current(
     else:
         return {"status": "failed", "message": "Insert failed"}
 
+@router.get("/todo")
+def get_todo(
+    date: Optional[str] = Query(None, format="date"),
+    user: auth.UserRecord = Depends(get_current_user)
+):
+    col_todo = db["todo"]
+    if date is None:
+        raise HTTPException(status_code=400, detail="Date query parameter is required")
+
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid date format")
+
+    try:
+        doc_todo = (
+            col_todo.find({"uid": user.uid, "date": date})
+            .sort("time", pymongo.DESCENDING)
+            .limit(1)
+            .next()
+        )
+    except:
+        today = datetime.now().date().isoformat()
+        doc_todo = {
+            "uid": user.uid,
+            "time": datetime.now(),
+            "date": today,
+            "todos": [],
+        }
+        col_todo.insert_one(doc_todo)
+    doc_todo = remove_objectId(doc_todo)
+    return doc_todo
+
 @router.post("/todo")
 def insert_todo(
-    data: TodoModel, user: auth.UserRecord = Depends(get_current_user)
+    data: TodoData, user: auth.UserRecord = Depends(get_current_user)
 ):
-    col_todo = db["current"]
+    col_todo = db.todo
 
     doc_todo = {
         "uid": user.uid,
-        "time": datetime.datetime.utcnow(),
-        "todos": data.todos,
+        "time": datetime.now(),
+        "date": data.date.split("T")[0],
+        "todos": [todo.model_dump() for todo in data.todos],
     }
 
     result = col_todo.insert_one(doc_todo)
@@ -83,7 +128,7 @@ def insert_todo(
         return {
             "status": "success",
             "message": "OK",
-            "todo": remove_objectId(doc_todo),
+            "todos": remove_objectId(doc_todo),
         }
     else:
         return {"status": "failed", "message": "Insert failed"}
